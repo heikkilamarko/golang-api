19bf6e98f57224044699c39e07c8135ec55e6171
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-01-24 00:06:25 +0200

    Update migrate_local.sh

--- db/migrate_local.sh
+++ db/migrate_local.sh
@@ -4,7 +4,6 @@ export $(grep -v '^#' ../../golang-api-secrets/db_local.env | xargs)
 
 docker run \
   --rm \
-  --network golang-api_golang-api-net \
   --mount type=bind,src=$(pwd)/migrations,dst=/migrations \
   migrate/migrate -path /migrations \
   -database $DB_CONNECTIONSTRING \
