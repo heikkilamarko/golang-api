5427d1e57717c84012a670716b4a9a54e67b7ec4
Marko Heikkilä <heikkila_marko@hotmail.com>
2023-06-07 15:28:17 +0300

    Migrate to slog and update deps.

--- api/go.mod
+++ api/go.mod
@@ -5,9 +5,9 @@ go 1.20
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
-	github.com/heikkilamarko/goutils v1.0.27
-	github.com/jackc/pgx/v4 v4.18.0
-	github.com/rs/zerolog v1.29.0
+	github.com/heikkilamarko/goutils v1.0.28
+	github.com/jackc/pgx/v4 v4.18.1
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
 
 require (
@@ -18,9 +18,6 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
--- api/internal/adapters/product_http_handlers.go
+++ api/internal/adapters/product_http_handlers.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/heikkilamarko/goutils"
-	"github.com/rs/zerolog"
+	"golang.org/x/exp/slog"
 )
 
 const (
@@ -35,10 +35,10 @@ const (
 
 type ProductHTTPHandlers struct {
 	app    *application.Application
-	logger *zerolog.Logger
+	logger *slog.Logger
 }
 
-func NewProductHTTPHandlers(app *application.Application, logger *zerolog.Logger) *ProductHTTPHandlers {
+func NewProductHTTPHandlers(app *application.Application, logger *slog.Logger) *ProductHTTPHandlers {
 	return &ProductHTTPHandlers{app, logger}
 }
 
@@ -48,7 +48,7 @@ func (h *ProductHTTPHandlers) GetProducts(w http.ResponseWriter, r *http.Request
 	q, err := parseGetProductsQuery(r)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteValidationError(w, err)
 		return
 	}
@@ -56,7 +56,7 @@ func (h *ProductHTTPHandlers) GetProducts(w http.ResponseWriter, r *http.Request
 	todos, err := h.app.Queries.GetProducts.Handle(r.Context(), q)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteInternalError(w, nil)
 		return
 	}
@@ -73,13 +73,13 @@ func (h *ProductHTTPHandlers) CreateProduct(w http.ResponseWriter, r *http.Reque
 	c, err := parseCreateProductCommand(r)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteValidationError(w, err)
 		return
 	}
 
 	if err := h.app.Commands.CreateProduct.Handle(r.Context(), c); err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteInternalError(w, nil)
 		return
 	}
@@ -91,7 +91,7 @@ func (h *ProductHTTPHandlers) GetProduct(w http.ResponseWriter, r *http.Request)
 	q, err := parseGetProductQuery(r)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteValidationError(w, err)
 		return
 	}
@@ -99,7 +99,7 @@ func (h *ProductHTTPHandlers) GetProduct(w http.ResponseWriter, r *http.Request)
 	product, err := h.app.Queries.GetProduct.Handle(r.Context(), q)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		switch err {
 		case ports.ErrNotFound:
 			goutils.WriteNotFound(w, nil)
@@ -116,13 +116,13 @@ func (h *ProductHTTPHandlers) UpdateProduct(w http.ResponseWriter, r *http.Reque
 	c, err := parseUpdateProductCommand(r)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteValidationError(w, err)
 		return
 	}
 
 	if err := h.app.Commands.UpdateProduct.Handle(r.Context(), c); err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		switch err {
 		case ports.ErrNotFound:
 			goutils.WriteNotFound(w, nil)
@@ -139,13 +139,13 @@ func (h *ProductHTTPHandlers) DeleteProduct(w http.ResponseWriter, r *http.Reque
 	c, err := parseDeleteProductCommand(r)
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteValidationError(w, err)
 		return
 	}
 
 	if err := h.app.Commands.DeleteProduct.Handle(r.Context(), c); err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		switch err {
 		case ports.ErrNotFound:
 			goutils.WriteNotFound(w, nil)
@@ -162,7 +162,7 @@ func (h *ProductHTTPHandlers) GetPriceRange(w http.ResponseWriter, r *http.Reque
 	pr, err := h.app.Queries.GetPriceRange.Handle(r.Context())
 
 	if err != nil {
-		h.logError(err)
+		h.logger.Error(err.Error())
 		goutils.WriteInternalError(w, nil)
 		return
 	}
@@ -170,16 +170,10 @@ func (h *ProductHTTPHandlers) GetPriceRange(w http.ResponseWriter, r *http.Reque
 	goutils.WriteOK(w, pr, nil)
 }
 
-// Utils
-
-func (h *ProductHTTPHandlers) logError(err error) {
-	h.logger.Error().Err(err).Send()
-}
-
 // Input parsers
 
 func parseGetProductsQuery(r *http.Request) (*query.GetProducts, error) {
-	errorMap := map[string]string{}
+	errs := map[string][]string{}
 
 	offset := 0
 	limit := limitMaxPageSize
@@ -189,19 +183,19 @@ func parseGetProductsQuery(r *http.Request) (*query.GetProducts, error) {
 	if value := r.FormValue(fieldPaginationOffset); value != "" {
 		offset, err = strconv.Atoi(value)
 		if err != nil || offset < 0 {
-			errorMap[fieldPaginationOffset] = errCodeInvalidOffset
+			errs[fieldPaginationOffset] = []string{errCodeInvalidOffset}
 		}
 	}
 
 	if value := r.FormValue(fieldPaginationLimit); value != "" {
 		limit, err = strconv.Atoi(value)
 		if err != nil || limit < 1 || limitMaxPageSize < limit {
-			errorMap[fieldPaginationLimit] = errCodeInvalidLimit
+			errs[fieldPaginationLimit] = []string{errCodeInvalidLimit}
 		}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	return &query.GetProducts{
@@ -211,73 +205,73 @@ func parseGetProductsQuery(r *http.Request) (*query.GetProducts, error) {
 }
 
 func parseCreateProductCommand(r *http.Request) (*command.CreateProduct, error) {
-	errorMap := map[string]string{}
+	errs := map[string][]string{}
 
 	product := &domain.Product{}
 	if err := json.NewDecoder(r.Body).Decode(product); err != nil {
-		errorMap[fieldRequestBody] = errCodeInvalidRequestBody
+		errs[fieldRequestBody] = []string{errCodeInvalidRequestBody}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	return &command.CreateProduct{Product: product}, nil
 }
 
 func parseGetProductQuery(r *http.Request) (*query.GetProduct, error) {
-	errorMap := map[string]string{}
+	errs := map[string][]string{}
 
 	id, err := strconv.Atoi(chi.URLParam(r, fieldID))
 	if err != nil {
-		errorMap[fieldID] = errCodeInvalidID
+		errs[fieldID] = []string{errCodeInvalidID}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	return &query.GetProduct{ID: id}, nil
 }
 
 func parseUpdateProductCommand(r *http.Request) (*command.UpdateProduct, error) {
-	errorMap := map[string]string{}
+	errs := map[string][]string{}
 
 	id, err := strconv.Atoi(chi.URLParam(r, fieldID))
 	if err != nil {
-		errorMap[fieldID] = errCodeInvalidID
+		errs[fieldID] = []string{errCodeInvalidID}
 	}
 
 	product := &domain.Product{}
 	if err := json.NewDecoder(r.Body).Decode(product); err != nil {
-		errorMap[fieldRequestBody] = errCodeInvalidRequestBody
+		errs[fieldRequestBody] = []string{errCodeInvalidRequestBody}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	if id != product.ID {
-		errorMap[fieldID] = errCodeInvalidID
+		errs[fieldID] = []string{errCodeInvalidID}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	return &command.UpdateProduct{Product: product}, nil
 }
 
 func parseDeleteProductCommand(r *http.Request) (*command.DeleteProduct, error) {
-	errorMap := map[string]string{}
+	errs := map[string][]string{}
 
 	id, err := strconv.Atoi(chi.URLParam(r, fieldID))
 	if err != nil {
-		errorMap[fieldID] = errCodeInvalidID
+		errs[fieldID] = []string{errCodeInvalidID}
 	}
 
-	if 0 < len(errorMap) {
-		return nil, goutils.NewValidationError(errorMap)
+	if 0 < len(errs) {
+		return nil, goutils.ValidationError{Errors: errs}
 	}
 
 	return &command.DeleteProduct{ID: id}, nil
